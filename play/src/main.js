815dab7d93792090ee850bee5f1986d46bc9081f
luminosity <thaddeus.k.escobarkirsch@msd549c.org>
2026-01-09 17:16:16 -0800

    Delete play directory
    deleted duplicate files

--- play/src/main.js
+++
@@ -1,6 +0,0 @@
-// main.js — entrypoint
-window.addEventListener('load', () => {
-  const canvasContainer = document.body;
-  const game = new Game({container: canvasContainer});
-  game.start();
-});
